beb069d3863f88b9e1d436194ca537da1e51066f
Core Explorer <coreexplorer8086@gmail.com>
2025-02-09 22:41:59 +0100

    quiet flag for wget in tests

--- testing/download.sh
+++ testing/download.sh
@@ -13,7 +13,7 @@ fi
 mkdir -p $HOME/.cdx/bin
 if [ ! -e $HOME/.cdx/node-latest-linux-x64/bin/node ] ; then 
 NODEVER=v23.7.0
-wget https://nodejs.org/dist/$NODEVER/node-$NODEVER-linux-x64.tar.xz
+wget -q https://nodejs.org/dist/$NODEVER/node-$NODEVER-linux-x64.tar.xz
 tar xf node-$NODEVER-linux-x64.tar.xz && rm node-$NODEVER-linux-x64.tar.xz
 mv node-$NODEVER-linux-x64 $HOME/.cdx/node-latest-linux-x64
 ln -sf $HOME/.cdx/node-latest-linux-x64/bin/node $HOME/.cdx/bin/node
