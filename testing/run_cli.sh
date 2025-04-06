39b02939ccebb2c2be0c958f264b61f2d963623e
Core Explorer <coreexplorer8086@gmail.com>
2025-04-06 17:58:13 +0200

    v0.0.12 - verify parsing of eh_frame and eh_frame_hdr against binutils
    matches binutils 2.42, 2.43 changes output for this

--- testing/run_cli.sh
+++ testing/run_cli.sh
@@ -13,16 +13,29 @@ which node
 node --version
 cdx readelf --version
 
-TOOLS="elfutils cdx" # binutils
 FILES="build/*-*/*-*/dummy-buildid.pie build/*-*/*-*/dummy-buildid.exe"
+#if false ; then
+TOOLS="elfutils cdx" # binutils
 OPTIONS="--segments --sections --debug-dump=info"
-
-
 for t in $TOOLS; do
-for f in $FILES; do
-for o in $OPTIONS; do
-./test_single_file.sh $t $o $f
+    for f in $FILES; do
+        for o in $OPTIONS; do
+            ./test_single_file.sh $t $o $f
+        done
+    done
+done
+#fi
 
-done
-done
+TOOLS="binutils cdx"
+OPTIONS="--debug-dump=frames"
+for t in $TOOLS; do
+    for f in $FILES; do
+        for o in $OPTIONS; do
+            if  dirname $f | grep -c arm && basename $f | grep -c exe; then
+            # no support for debug_frame at the moment
+            continue;
+            fi
+            ./test_single_file.sh $t $o $f
+        done
+    done
 done
